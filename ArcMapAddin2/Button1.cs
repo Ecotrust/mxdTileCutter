82779e01f201e4845b0e266baca4ea0030175b1c
Matthew Perry <mperry@ecotrust.org>
2013-03-27 08:38:36 -0700

    several failed attempts at setting transparency on pngs

--- ArcMapAddin2/Button1.cs
+++ ArcMapAddin2/Button1.cs
@@ -26,10 +26,50 @@ namespace ArcMapAddin1
             ESRI.ArcGIS.Carto.IActiveView activeView = mxDocument.ActiveView;
             ESRI.ArcGIS.Carto.IMap map = activeView.FocusMap;
 
-            // Set up export object and pixel coordinates
+            // Set up export object and tile pixel coordinates
             int tileSizeX = 256;
             int tileSizeY = 256;
-            ESRI.ArcGIS.Output.IExport export = new ESRI.ArcGIS.Output.ExportPNGClass();
+
+            /*
+            ESRI.ArcGIS.Output.ExportPNG pngexport = new ESRI.ArcGIS.Output.ExportPNGClass();
+            pngexport.BackgroundColor.NullColor = true;
+            ESRI.ArcGIS.Output.IExport export = (ESRI.ArcGIS.Output.IExport)pngexport;
+            */
+
+            ESRI.ArcGIS.Output.ExportPNG pngexport = new ESRI.ArcGIS.Output.ExportPNGClass();
+            ESRI.ArcGIS.Display.IColor tcolor = new ESRI.ArcGIS.Display.RgbColorClass();
+            ((IRgbColor)tcolor).Red = 255;
+            ((IRgbColor)tcolor).Green = 0;
+            ((IRgbColor)tcolor).Blue = 0;
+            ((IRgbColor)tcolor).Transparency = 0;  // Doesn't work?
+            pngexport.BackgroundColor = tcolor;
+            ((IExportPNG)pngexport).TransparentColor = tcolor;
+            ESRI.ArcGIS.Output.IExport export = (ESRI.ArcGIS.Output.IExport)pngexport;
+            
+            
+            // ***************************
+            // TODO set transparency, 32 bit png
+            // ESRI.ArcGIS.Carto.esriImageFormat.esriImagePNG32;            
+            //ESRI.ArcGIS.Output.IExport export = new ESRI.ArcGIS.Output.ExportPNGClass();
+            //ESRI.ArcGIS.Output.IExport export = (ESRI.ArcGIS.Output.IExport)xpk;
+            //export.ImageType = (ESRI.ArcGIS.Output.esriExportImageType)ESRI.ArcGIS.Carto.esriImageFormat.esriImagePNG32;
+            //
+            //export.TransparentColor.Transparency = (byte)255;
+            //export.BackgroundColor.Transparency = (byte)0;
+            //export.BackgroundColor.
+
+            /*
+             * IExportPNG pngExport = new ExportPNGClass();
+                    export = (IExport)pngExport;
+
+             * 
+             *             pngExport.TransparentColor.Transparency = 0;
+            
+            ESRI.ArcGIS.Output.IExport export = (ESRI.ArcGIS.Output.IExport)pngExport;break;
+             */
+
+            //export.ImageType = (ESRI.ArcGIS.Output.esriExportImageType)32;
+            // ***************************
             ESRI.ArcGIS.esriSystem.tagRECT exportRECT;
             exportRECT.left = 0;
             exportRECT.top = 0;
@@ -51,34 +91,36 @@ namespace ArcMapAddin1
                 ESRI.ArcGIS.Carto.ILayer layer = map.get_Layer(lyrnum);
                 layer.Visible = true;
 
-                export.ExportFileName = "e:\\workspace\\test_addin\\" + layer.Name + ".png";
-   
-                // Refresh and Zoom to the layer
-                // Use FullExtent instead of Extent to make the extent independent of the activeView ratio
-
-                // TODO instead of AreaOfInterest, try a hardcoded extent
-                //activeView.FullExtent = layer.AreaOfInterest;
-
+                // Set extents
                 //ESRI.ArcGIS.Geometry.Envelope aoi = layer.AreaOfInterest;
                 ESRI.ArcGIS.Geometry.IEnvelope aoi = new ESRI.ArcGIS.Geometry.EnvelopeClass();
-                //aoi.PutCoords(exportRECT.left, exportRECT.top, exportRECT.right, exportRECT.bottom);
-                aoi.PutCoords(-13832493.1523, 5909673.4917, -13232493.1523, 5309673.4917);
-                aoi.SpatialReference = map.SpatialReference;
-                // TODO aoi spatial reference
-                activeView.FullExtent = aoi;
+
+                /* TODO Loop through zoom levels, rows, cols 
+                 * 
+                 */
+                    export.ExportFileName = "e:\\workspace\\test_addin\\" + layer.Name + ".png";
+                    //aoi.PutCoords(exportRECT.left, exportRECT.top, exportRECT.right, exportRECT.bottom);
+                    aoi.PutCoords(-13832493.1523, 5909673.4917, -13232493.1523, 5309673.4917);
+                    aoi.SpatialReference = map.SpatialReference; // TODO aoi spatial reference == mercator?
+                    // Use FullExtent instead of Extent to make the extent independent of the activeView ratio
+                    activeView.FullExtent = aoi;
                 
-                // Export
-                System.Int32 hDC = export.StartExporting();
-                activeView.Output(hDC, (System.Int16)export.Resolution, ref exportRECT, null, null); // Explicit Cast and 'ref' keyword needed 
-                export.FinishExporting();
-                export.Cleanup();
+                    // Export
+                    System.Int32 hDC = export.StartExporting();
+                    activeView.Output(hDC, (System.Int16)export.Resolution, ref exportRECT, null, null); // Explicit Cast and 'ref' keyword needed 
+                    export.FinishExporting();
+                    export.Cleanup();
 
                 // Turn it off
                 layer.Visible = false;
             }
 
-            activeView.Refresh();
+            // Turn ON all layers
+            for (int i = 0; i < map.LayerCount; i++)
+                map.get_Layer(i).Visible = true;
+            
             map.DelayDrawing(false);  
+            activeView.Refresh();
         }
 
         protected override void OnUpdate()
