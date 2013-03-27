968f736008d1dd7841d607c54e3d483a9ca1366b
Matthew Perry <mperry@ecotrust.org>
2013-03-26 21:14:03 -0700

    set up export object up front and be smarter about actions within loop

--- ArcMapAddin2/Button1.cs
+++ ArcMapAddin2/Button1.cs
@@ -20,56 +20,25 @@ namespace ArcMapAddin1
         {
         }
 
-        ///<summary>Creates a tile 256x256.</summary>
-        ///
-        ///<param name="activeView">An IActiveView interface</param>
-        ///<param name="pathFileName">A System.String that the path and filename of the JPEG you want to create. Example: "C:\temp\hiResolutionTest.jpg"</param>
-        /// 
-        ///<returns>A System.Boolean indicating the success</returns>
-        /// 
-        ///<remarks></remarks>
-        public System.Boolean CreateTileFromActiveView(ESRI.ArcGIS.Carto.IActiveView activeView, System.String pathFileName, int tileSizeX, int tileSizeY)
-        {
-            //parameter check
-            if (activeView == null || !(pathFileName.EndsWith(".png")))
-            {
-                return false;
-            }
-            ESRI.ArcGIS.Output.IExport export = new ESRI.ArcGIS.Output.ExportPNGClass();
-            export.ExportFileName = pathFileName;
-
-            export.Resolution = 300;
-
-            //ESRI.ArcGIS.Display.tagRECT exportRECT; // This is a structure
-            ESRI.ArcGIS.esriSystem.tagRECT exportRECT;
-            exportRECT.left = 0;
-            exportRECT.top = 0;
-            exportRECT.right = tileSizeX;
-            exportRECT.bottom = tileSizeY;
-
-            // Set up the PixelBounds envelope to match the exportRECT
-            ESRI.ArcGIS.Geometry.IEnvelope envelope = new ESRI.ArcGIS.Geometry.EnvelopeClass();
-            envelope.PutCoords(exportRECT.left, exportRECT.top, exportRECT.right, exportRECT.bottom);
-            export.PixelBounds = envelope;
-
-            System.Int32 hDC = export.StartExporting();
-
-            activeView.Output(hDC, (System.Int16)export.Resolution, ref exportRECT, null, null); // Explicit Cast and 'ref' keyword needed 
-            export.FinishExporting();
-            export.Cleanup();
-
-            return true;
-        }
-
         protected override void OnClick()
         {            
             ESRI.ArcGIS.ArcMapUI.IMxDocument mxDocument = ArcMap.Application.Document as ESRI.ArcGIS.ArcMapUI.IMxDocument; // Dynamic Cast
             ESRI.ArcGIS.Carto.IActiveView activeView = mxDocument.ActiveView;
             ESRI.ArcGIS.Carto.IMap map = activeView.FocusMap;
 
+            // Set up export object and pixel coordinates
             int tileSizeX = 256;
             int tileSizeY = 256;
-            
+            ESRI.ArcGIS.Output.IExport export = new ESRI.ArcGIS.Output.ExportPNGClass();
+            ESRI.ArcGIS.esriSystem.tagRECT exportRECT;
+            exportRECT.left = 0;
+            exportRECT.top = 0;
+            exportRECT.right = tileSizeX;
+            exportRECT.bottom = tileSizeY;
+            ESRI.ArcGIS.Geometry.IEnvelope envelope = new ESRI.ArcGIS.Geometry.EnvelopeClass();
+            envelope.PutCoords(exportRECT.left, exportRECT.top, exportRECT.right, exportRECT.bottom);
+            export.PixelBounds = envelope;
+
             map.DelayDrawing(true);
 
             // Turn off all layers
@@ -81,12 +50,19 @@ namespace ArcMapAddin1
                 // Turn on the layer of interest
                 ESRI.ArcGIS.Carto.ILayer layer = map.get_Layer(lyrnum);
                 layer.Visible = true;
+
+                export.ExportFileName = "e:\\workspace\\test_addin\\" + layer.Name + ".png";
    
                 // Refresh and Zoom to the layer
                 // Use FullExtent instead of Extent to make the extent independent of the activeView ratio
+                // TODO instead of AreaOfInterest, try a hardcoded extent
                 activeView.FullExtent = layer.AreaOfInterest;
-                //TODO slugify name
-                CreateTileFromActiveView(activeView, "e:\\workspace\\test_addin\\" + layer.Name + ".png", tileSizeX, tileSizeY);
+                
+                // Export
+                System.Int32 hDC = export.StartExporting();
+                activeView.Output(hDC, (System.Int16)export.Resolution, ref exportRECT, null, null); // Explicit Cast and 'ref' keyword needed 
+                export.FinishExporting();
+                export.Cleanup();
 
                 // Turn it off
                 layer.Visible = false;
